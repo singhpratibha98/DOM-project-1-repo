a55ca9d65f58570eae654cf3ba23739ee2d10f52
pratibha singh <ps6977616@gmail.com>
2023-10-03 12:51:20 +0530

    done

---
+++ style.js
@@ -0,0 +1,10 @@
+var a= document.getElementById("hero");
+
+a.style.backgroundColor="yellow";
+a.style.margin="20px";
+a.style.padding ="10px";
+a. style.fontSize ="18px";
+a.style.fontWeight ="bold";
+a.style.height ="200px";
+a.style.width ="300px";
+a.style.color="blue";
